be4e4da493b8cae253df83cc185b4a34340eb22c
Justin Leitgeb <justin@stackbuilders.com>
2011-03-17 19:02:37 -0400

    New Rails 3 app

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-HackchangeDemo::Application.config.secret_token = '86bcd221578233cdeb1f4beb2d2822779234fac5dab0033dca8344b1f670c52a4b7d2e685be37fba5fb1749471c8a093feb237fb0eb68dd010f3e73f7d0a2fce'
+HackchangeDemo::Application.config.secret_token = '01b92b0e8e7da60bb0cab44ded00ffa57192f62bbec7cb5ab744389d52aa9098855b3a10038d14042c4baece7c3d96cf37348722aebcbef735b05dc01f46a294'
