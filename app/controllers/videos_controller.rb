27759b10bad066d3b1c0316ddb1b1e3a9f65fc92
Paul Dix <paul@pauldix.net>
2011-04-21 20:16:06 -0400

    week 2 content added. BOOM!

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,10 +1,25 @@
 class VideosController < ApplicationController
   def new
-    
+
+  end
+
+  def edit
+    @video = Video.find(params[:id])
+  end
+
+  def update
+    @video = Video.find(params[:id])
+    @video.update_attributes(params[:video])
+    redirect_to root_path
   end
 
   def create
     Video.create(params[:video])
+    redirect_to root_path
+  end
+
+  def destroy
+    Video.find(params[:id]).destroy
     redirect_to root_path
   end
 end
